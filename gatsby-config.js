894a50f0e44dfe0a2bf67c0a55d97b5758752c41
Zabana <karim.cheurfi@gmail.com>
2019-11-21 17:37:09 +0100

    wip: working on the header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Karim Cheurfi`,
+    description: `This is a super cool meta description that I will have to change later on`,
+    author: "",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
---
+++ src/components/home/HomeHeader.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "../../styles/home/header.sass"
+
+const HomeHeader = () => (
+  <header className="home__header-splash">
+    <div className="home__header-splash__container">
+      <h1 className="home__header-splash__logo">カリム</h1>
+      <span className="home__header-splash__tagline">Oh hello, Welcome !</span>
+    </div>
+  </header>
+)
+
+export default HomeHeader
