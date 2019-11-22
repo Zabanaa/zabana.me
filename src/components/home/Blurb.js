033fe47edae3a0d686f17789106af0e901c4feed
Zabana <karim.cheurfi@gmail.com>
2019-11-22 17:33:15 +0100

    made blurb section fullscreen

--- src/components/home/Blurb.js
+++ src/components/home/Blurb.js
@@ -34,8 +34,8 @@ const HomeHeader = () => (
         . I've been writing code professionally for <strong>5 years</strong>{" "}
         now, and as a hobby since I was 8. My languages of choice are{" "}
         <strong className="fuschia">Python and Node.js</strong> (in that order)
-        but I have a sweet spot in my heart for C (I should probably mention
-        that I've dabbled in Golang and PHP in the past,{" "}
+        but I have a sweet spot in my heart for C (I should also probably
+        mention that I've dabbled in Golang and PHP in the past,{" "}
         <span className="strike">because I'm cool like that</span>).
       </p>
       <p>
