9a0267c0b990124b3ed2d5ff44079387164051c8
Zabana <karim.cheurfi@gmail.com>
2019-11-22 11:41:03 +0100

    added glitch effect to main title

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -4,7 +4,9 @@ import "../../styles/home/header.sass"
 const HomeHeader = () => (
   <header className="home__header-splash">
     <div className="home__header-splash__container">
-      <h1 className="home__header-splash__logo">カリム</h1>
+      <h1 data-name="カリム" className="home__header-splash__logo">
+        カリム
+      </h1>
       <span className="home__header-splash__tagline">Oh hello, Welcome !</span>
     </div>
   </header>
