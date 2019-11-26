6b736d9f07623bd01e3c343310335a95852e8c71
Zabana <karim.cheurfi@gmail.com>
2019-11-26 16:10:05 +0100

    created footer component

---
+++ src/components/home/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer__container">
+        <span className="footer__logo"> カリム</span>
+        <p>Made with code at my local library.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import Layout from "../components/layout"
 import HomeHeader from "../components/home/HomeHeader"
 import Blurb from "../components/home/Blurb"
 import ProjectsList from "../components/home/ProjectsList"
+import Contact from "../components/home/Contact"
+import Footer from "../components/home/Footer"
 
 const IndexPage = () => (
   <Layout>
     <HomeHeader />
     <Blurb />
     <ProjectsList />
+    <Contact />
+    <Footer />
   </Layout>
 )
 
