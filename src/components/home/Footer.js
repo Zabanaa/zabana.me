ebf0bfe1b95d3f386a113545aae899959c84508c
Zabana <karim.cheurfi@gmail.com>
2019-11-26 16:59:50 +0100

    style footer component

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,10 +1,14 @@
 import React from "react"
+import "../../styles/home/footer.sass"
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer__container">
-        <span className="footer__logo"> カリム</span>
+        <span className="footer__logo" data-logo="カリム">
+          {" "}
+          カリム
+        </span>
         <p>Made with code at my local library.</p>
       </div>
     </footer>
