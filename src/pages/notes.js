30546ca1412497f538499901d7f03f4922dad5a7
Zabana <karim.cheurfi@gmail.com>
2019-11-28 16:52:04 +0100

    added footer to blog posts page

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/blog/Header"
 import PostList from "../components/blog/PostList"
+import Footer from "../components/blog/Footer"
 
 const NotesPage = () => {
   return (
     <Layout>
       <Header />
       <PostList />
+      <Footer />
     </Layout>
   )
 }
