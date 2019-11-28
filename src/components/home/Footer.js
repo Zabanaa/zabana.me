c4362c7d1e6642ab50547c75770ba82a63b589d9
Zabana <karim.cheurfi@gmail.com>
2019-11-28 16:51:14 +0100

    added classes to footer elements

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -3,13 +3,13 @@ import "../../styles/home/footer.sass"
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer__container">
-        <span className="footer__logo" data-logo="カリム">
+    <footer className="home-footer">
+      <div className="home-footer__container">
+        <span className="home-footer__logo" data-logo="カリム">
           {" "}
           カリム
         </span>
-        <p className="footer__about">
+        <p className="home-footer__about">
           Made while listening to Eddie Murphy's albums.
         </p>
       </div>
